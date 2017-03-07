27edab94e30077f75c0847839a2e6a139e3e9261
Gabriela Zamudio <gabriela.osu@gmail.com>
2017-03-06 16:43:37 -0800

    add pseudo code in js file

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// When the user submits the form
+
+	// Prevent the page from refreshing
+
+
+	// Form Validation
+
+	// 1) Get the value from the #authorSelect field and save in variable author
+
+	// 2) Get the value from the #nounInput field and save in variable noun
+
+	// 3) Get the value from the #verbInput field and save in variable verb
+
+	// 4) Generate error validation (Error class)
+
+	// 5) Add error validation if input are empty 	
+
+
+	//  Quote display
+
+		// 1) Hide quote unless selection is made
+	
+		// 2) Fade in the quote by the selected author
+
+		// 3) Update the text of the element with the class .noun to the text stored in the noun variable.	
+
+		// 4) Update the text of the element with the class .verb to the text stored in the verb variable.	
