e84b29a96880bd6c5cb6f937f912af30293a1591
Gabriela Zamudio <gabriela.osu@gmail.com>
2017-03-06 16:49:08 -0800

    add gathering data from user input

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 // When the user submits the form
 
-	// Prevent the page from refreshing
+$('form').on('submit', function(e) {
 
+	// Prevent the page from refreshing
+	e.preventDefault();
 
 	// Form Validation
 
 	// 1) Get the value from the #authorSelect field and save in variable author
 
+	var author = $('#authorSelect').val();
+	console.log(author);
+
 	// 2) Get the value from the #nounInput field and save in variable noun
 
+	var noun = $('#nounInput').val();
+	console.log(noun);
+
 	// 3) Get the value from the #verbInput field and save in variable verb
+	var verb = $('#verbInput').val();
+	console.log(verb);
 
 	// 4) Generate error validation (Error class)
 
@@ -25,3 +35,5 @@
 		// 3) Update the text of the element with the class .noun to the text stored in the noun variable.	
 
 		// 4) Update the text of the element with the class .verb to the text stored in the verb variable.	
+
+});
